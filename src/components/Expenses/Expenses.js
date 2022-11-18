4f0527ccfcbc8e14b90cab6894837dd942bd2f0e
serkanursavas <serkan.ursavas@icloud.com>
2022-11-18 09:59:23 +0300

    added conditional output

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,12 +12,20 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     }
 
+    const filteredExpenses = props.array.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear
+    })
+
+    let expensesContent = <p>No expenses found.</p>
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)
+    }
+
     return (
         <Card className="expenses">  
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>    
-            {props.array.filter((expense) => {
-                return expense.date.getFullYear().toString() === filteredYear
-                }).map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)}
+            {expensesContent}
         </Card>
     )
 }
