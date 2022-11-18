6b60e0bfb01227b11481fe4315658f329762449e
serkanursavas <serkan.ursavas@icloud.com>
2022-11-18 10:34:00 +0300

    seperated expenses via new ExpensesList comp.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import './Expenses.css'
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import { useState } from 'react';
 
 const Expenses = (props) => {
@@ -16,17 +16,15 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear
     })
 
-    let expensesContent = <p>No expenses found.</p>
-
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)
-    }
+    
 
     return (
-        <Card className="expenses">  
-            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>    
-            {expensesContent}
-        </Card>
+        <li>
+            <Card className="expenses">  
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>    
+                <ExpensesList items={filteredExpenses }/>
+            </Card>
+        </li>
     )
 }
 
